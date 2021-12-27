03abc343abaabb67b9657527fa9e9f6780d655d5
Projects1234w <87812544+Projects1234w@users.noreply.github.com>
2021-12-27 10:30:13 +0530

    Add files via upload

---
+++ bedroom.js
@@ -0,0 +1,30 @@
+staus=""
+objects=[]
+function preload(){
+img=loadImage("20211030_193224.jpg")
+}
+function setup(){
+canvas=createCanvas(640,420);
+canvas.center();
+objectDetector=ml5.objectDetector("cocossd",modelLoaded);
+document.getElementById("status").innerHTML="Status : Detecting Objects"
+}
+function draw(){
+    image(img,0,0,640,420)
+for(i=0;i<object.length;i++){
+    text(object.label);
+}
+}
+function modelLoaded(){
+    console.log("Model Loaded!");
+
+}
+function gotResult(error,results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+}
+function back(){
+window.location("index.html");
+}
---
+++ shelf.js
@@ -0,0 +1,26 @@
+status=""
+function preload(){
+    img=loadImage("20211030_193621.jpg")
+    }
+    function setup(){
+    canvas=createCanvas(640,420);
+    canvas.center();
+    objectDetector=ml5.objectDetector("cocossd",modelLoaded);
+    document.getElementById("status").innerHTML="Status : Detecting Objects"
+    }
+    function draw(){
+        image(img,0,0,640,420)
+    }
+    function modelLoaded(){
+        console.log("Model Loaded!");
+    
+    }
+    function gotResult(error,results){
+        if(error){
+            console.log(error);
+        }
+        console.log(results);
+    }
+    function back(){
+    
+    }
